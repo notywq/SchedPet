9db7d1fe0bd3e90b0e98bcd68ea2d5af8d4fdb9c
Baron Dimaranan <barondimaranan@gmail.com>
2016-12-15 09:39:12 +0800

    Added Splash Screen

---
+++ app/src/main/java/com/mobidev/carmelalouise/schedpet/page/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.mobidev.carmelalouise.schedpet.page;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mobidev.carmelalouise.schedpet.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
